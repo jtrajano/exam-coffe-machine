8cf217095986740a14cf6a4605e0397a0f166e6b
jtrajano <jlt212000@yahoo.com>
2026-05-01 22:57:31 +0800

    Initial Commit

---
+++ Endpoints.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeMachine;
+
+public static class Endpoints
+{
+    public static void MapEndpoints(this WebApplication app)
+    {
+        app.MapGet("/brew-cofee", BrewCoffeAsync);
+    }
+
+
+    public static async Task<IResult> BrewCoffeAsync()
+    {
+        DateTime dateNow = DateTime.UtcNow;
+        
+        if(dateNow.Month == 4 && dateNow.Day == 1)
+                return Results.StatusCode(StatusCodes.Status418ImATeapot);
+
+        return Results.Ok(new { message= "Your piping hot coffee is ready", prepared= dateNow });
+    }
+
+}
